4f5775fc8a289acad19501926f20317b02ce61fd
Sam Potter <sam_pot_97@hotmail.com>
2015-11-11 00:28:49 +0000

    View port placement
    Split screen view port placement.

--- Square Off Unity/Assets/Scripts/Camera/Follow.cs
+++ Square Off Unity/Assets/Scripts/Camera/Follow.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Follow : MonoBehaviour {
 
+    private static uint camera_track = 0;
+
     public GameObject target;
     public float distance = 3f;
     public Vector3 direction = Vector3.back;
@@ -13,6 +15,32 @@ public class Follow : MonoBehaviour {
 
     void Start() {
         target.GetComponent<PlayerController>().movement_frame = transform;
+        setupViewPort();
+    }
+
+    private void setupViewPort() {
+        Camera cam = GetComponent<Camera>();
+
+        switch (camera_track)
+        {
+            case 0:
+                cam.rect = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
+                break;
+            case 1:
+                cam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                break;
+            case 2:
+                cam.rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
+                break;
+            case 3:
+                cam.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
+                break;
+            default:
+                cam.rect = new Rect(0.4f, 0.4f, 0.2f, 0.2f);
+                break;
+        }
+
+        camera_track++;
     }
 
     void Update() {
