8e0bad1da085c622a1b6390b4be0399cfba31e85
Sam Potter <sam_pot_97@hotmail.com>
2015-11-13 21:26:23 +0000

    Menu additions
    Added Settings submenu and added functionality to Settings and Quit
    button.

---
+++ Square Off Unity/Assets/Scripts/UI/TogglePanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePanel : MonoBehaviour {
+
+    public GameObject toggled;
+
+    public void toggle() {
+        toggled.SetActive(!toggled.activeSelf);
+    }
+}
